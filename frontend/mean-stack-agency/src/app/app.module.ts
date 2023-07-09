6a1ff3308c6407c528e13b25823fae16ebae1740
alexanddretgneto <alexanddretgneto@gmail.com>
2023-07-09 04:09:03 -0300

    aprendendo componentes e modulos

--- frontend/mean-stack-agency/src/app/app.module.ts
+++ frontend/mean-stack-agency/src/app/app.module.ts
@@ -3,18 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //importando para trabalhar com o NgModel
 import { FormsModule} from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+import { UserModule } from './user/user.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     //declarando o FormModule
-    FormsModule
+    FormsModule,
+
+    //esta declaração foi criada automatica
+    // com o comando ng g module user  --module=app
+    UserModule
 
   ],
   providers: [],
